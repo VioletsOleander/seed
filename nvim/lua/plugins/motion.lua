ad635d3322a4ef21821f6114ec4b54bb8ba04acb
VioletsOleander <1377232072@qq.com>
2026-03-13 22:39:41 +0800

    nvim(plugin): delete sneak, let flash-zh lazy

--- nvim/lua/plugins/motion.lua
+++ nvim/lua/plugins/motion.lua
@@ -71,7 +71,6 @@ return {
 		"rainzm/flash-zh.nvim",
 		dependencies = { "folke/flash.nvim" },
 		lazy = true,
-		event = "VeryLazy",
 		keys = {
 			{
 				"gz",
@@ -81,45 +80,6 @@ return {
 				end,
 				desc = "Flash between Chinese",
 			},
-		},
-	},
-	{
-		"justinmk/vim-sneak",
-		enabled = false,
-		dependencies = { "tpope/vim-repeat" },
-		init = function()
-			-- use smartcase and ignorecase
-			vim.g["sneak#use_ic_scs"] = 1
-		end,
-		config = function()
-			local api = vim.api
-
-			-- disable sneak-s and sneak-S
-			vim.keymap.set({ "n", "x" }, "<Plug>(Disable-Sneak-s)", "<Plug>Sneak_s")
-			vim.keymap.set({ "n", "x" }, "<Plug>(Disable-Sneak-S)", "<Plug>Sneak_S")
-
-			-- disable highlight
-			local function clear_sneak_highlight()
-				api.nvim_set_hl(0, "Sneak", { link = "None" })
-				api.nvim_set_hl(0, "SneakCurrent", { link = "None" })
-			end
-
-			clear_sneak_highlight()
-
-			api.nvim_create_autocmd("ColorScheme", {
-				callback = clear_sneak_highlight,
-			})
-			api.nvim_create_autocmd("User", {
-				pattern = "SneakLeave",
-				callback = clear_sneak_highlight,
-			})
-		end,
-		event = "VeryLazy",
-		keys = {
-			{ "f", "<Plug>Sneak_f", mode = { "n", "x", "o" }, desc = "Sneak Forward to" },
-			{ "F", "<Plug>Sneak_F", mode = { "n", "x", "o" }, desc = "Sneak Backward to" },
-			{ "t", "<Plug>Sneak_t", mode = { "n", "x", "o" }, desc = "Sneak Forward till" },
-			{ "T", "<Plug>Sneak_T", mode = { "n", "x", "o" }, desc = "Sneak Backward till" },
 		},
 	},
 }
