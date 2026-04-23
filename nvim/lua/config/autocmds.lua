c8727a3ec0a7e06ceff0cb5fb532ef43b336e1d6
VioletsOleander <1377232072@qq.com>
2026-04-23 21:27:26 +0800

    nvim(config): simplify autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,53 +1,31 @@
-if not vim.g.vscode then
-	local au = vim.api.nvim_create_autocmd
-	local function augroup(name)
-		return vim.api.nvim_create_augroup("user." .. name, { clear = true })
-	end
-
-	-- Completion
-	local completion_group = augroup("completion")
-	-- cmdline autocompletion
-	au("CmdlineChanged", {
-		group = completion_group,
-		pattern = { ":", "/", "?" },
-		callback = function()
-			vim.fn.wildtrigger()
-		end,
-		desc = "Automatically show popup menu when typing in cmd line",
-	})
-
-	local lsp_group = augroup("lsp")
-	-- set kemaps when lsp attaches to buffer
-	au("LspAttach", {
-		group = lsp_group,
-		pattern = "*",
-		callback = function(event)
-			local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
-			if client:supports_method("textDocument/completion") then
-				vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-			end
-
-			local map = function(mode, lhs, rhs, desc)
-				vim.keymap.set(mode, lhs, rhs, { buffer = event.buf, desc = "LSP: " .. desc })
-			end
-
-			local buf = vim.lsp.buf
-			map("n", "gD", buf.declaration, "Go to Declaration")
-
-			map("n", "K", buf.hover, "Hover Documentation")
-			map("i", "<C-k>", buf.signature_help, "Signature Help")
-
-			map("n", "<Leader>r", buf.rename, "Rename Symbol")
-			map({ "n", "v" }, "<Leader>a", buf.code_action, "Code Action")
-
-			local diagnostic = vim.diagnostic
-			map("n", "<Leader>cd", diagnostic.open_float, "Show Line Diagnostics")
-			map("n", "[d", function()
-				diagnostic.jump({ count = 1, float = true })
-			end, "Prev Diagnostic")
-			map("n", "]d", function()
-				diagnostic.jump({ count = -1, float = true })
-			end, "Next Diagnostic")
-		end,
-	})
+if vim.g.vscode then
+	return nil
 end
+
+local au = vim.api.nvim_create_autocmd
+local aug = vim.api.nvim_create_augroup
+
+-- Completion
+local completion_group = aug("user.completion", { clear = true })
+
+-- cmdline autocompletion
+au("CmdlineChanged", {
+	group = completion_group,
+	pattern = { ":", "/", "?" },
+	callback = function()
+		vim.fn.wildtrigger()
+	end,
+	desc = "Automatically show popup menu when typing in cmd line",
+})
+
+-- lsp autocompletion
+au("LspAttach", {
+	group = completion_group,
+	pattern = "*",
+	callback = function(event)
+		local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+		end
+	end,
+})
